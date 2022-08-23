f8713ab1c54a258fe3ba8fdf9e35d226f9ac71e8
Vignesh Rajagopalan <me@aarvay.com>
2022-08-23 09:56:08 +0530

    Fix link bleed in safari
    Had to imperatively create routes to make entire row clickable. There's a know LinkOverlay bug.
    Refer: https://github.com/chakra-ui/chakra-ui/issues/3832

--- components/PoolsTable/Row.tsx
+++ components/PoolsTable/Row.tsx
@@ -1,25 +1,26 @@
 import { FC } from "react";
-import Link from "next/link";
-import { LinkBox, LinkOverlay, Td } from "@chakra-ui/react";
+import { Td, Tr } from "@chakra-ui/react";
 import { TokenPair } from "../shared/TokenPair";
+import { useRouter } from "next/router";
 
 interface Props {
   data?: any;
 }
 
 export const Row: FC<Props> = ({ data }) => {
+  const router = useRouter();
+
   return (
-    <LinkBox as="tr" _hover={{ color: "darkgray" }}>
+    <Tr
+      _hover={{ color: "darkgray", cursor: "pointer" }}
+      onClick={() => router.push(`/pools/${data.id}`)}
+    >
       <Td>
-        <Link href={`/pools/${data.id}`} passHref>
-          <LinkOverlay>
-            <TokenPair pool={data} />
-          </LinkOverlay>
-        </Link>
+        <TokenPair pool={data} />
       </Td>
       <Td isNumeric>{data.txCount}</Td>
       <Td isNumeric>{data.tvl}</Td>
       <Td isNumeric>{data.volume}</Td>
-    </LinkBox>
+    </Tr>
   );
 };
